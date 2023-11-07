fe9ed6f45f399c242ef221d6d0c0d5a19a72e568
mwssy <mokmail070@gmail.com>
2023-11-07 16:17:25 +0900

    헤더 작업 2차

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,16 +4,40 @@ import styled from "styled-components";
 function Header() {
     return (
         <Container>
-            <Link to="/">홈</Link>
-            <Link to="/movie">서브</Link>
+            <Cell className="left">
+                <Link to="/">
+                    <Img src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg" alt="로고" />
+                </Link>
+                <Link to="/movie">서브1</Link>
+                <Link to="/movie">서브2</Link>
+                <Link to="/movie">서브3</Link>
+            </Cell>
         </Container>
     );
 }
 
 export default Header;
 
-const Container = styled.h1`
+const Container = styled.div`
     justify-content: space-between;
     display: flex;
     align-items: center;
+    margin: 0 2.5rem;
+    padding: 0.5rem 0;
+`
+
+const Cell = styled.div`
+    display: flex;
+    align-items: center;
+    &.left {
+        gap: 3rem;
+        font-size: 1.3rem;
+        cursor: pointer;
+    }
+`
+
+const Img = styled.img`
+    width: 80px;
+    cursor: pointer;
+    display: block;
 `
